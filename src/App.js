8581d0bfe47681c3c4e4dc7f0ffb422503b66e0b
dtsankov <dimitur.tsankov@gmail.com>
2023-11-10 20:20:30 +0200

    This commit adds component html and css skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalog } from "./components/Catalog/Catalog";
+
 
 function App() {
   return (
@@ -8,9 +13,14 @@ function App() {
 		<Header/>
 
 		<main id="main-content">
-			<Home/>
+			{/* <Home/> */}
+			{/* <Login/> */}
+			{/* <Register/> */}
+			{/* <CreateGame/> */}
+			{/* <Catalog/> */}
+
         </main>
-		
+
 		<Footer/>
     </div>
   );
